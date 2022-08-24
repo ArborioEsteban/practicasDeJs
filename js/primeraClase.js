1c881214755650e1402342cfca15ad743b3e3cf6
Esteban Arborio <estebanarborio@gmail.com>
2022-08-24 20:26:17 -0300

    Primer Commit

---
+++ js/primeraClase.js
@@ -0,0 +1,9 @@
+// console.log("Hola Mundo");
+
+const nombre = prompt("Escriba su nombre");
+console.log(nombre);
+document.write(nombre);
+// prompt ventana q pide ingresar dato
+// let y const para declarar variables, let son para variables q pueden modificarse en el futuro , const para valores q seran constantes , que nunca cambiaran
+// 
+
